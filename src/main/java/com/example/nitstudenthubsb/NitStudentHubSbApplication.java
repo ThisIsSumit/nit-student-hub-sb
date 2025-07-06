f1750228e29fb5b25188cad1df4e6ff2219e7aae
ThisIsSumit <sumitkumar453827@gmail.com>
2025-07-06 13:56:10 +0530

    Initial commit

---
+++ src/main/java/com/example/nitstudenthubsb/NitStudentHubSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.nitstudenthubsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitStudentHubSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NitStudentHubSbApplication.class, args);
+    }
+
+}
